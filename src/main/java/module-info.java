1d6988f2eaefe70ca29712304abf84a271226432
zemmelmootez <zemmel.mootez@gmail.com>
2023-04-03 20:44:44 +0100

    added

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.example.projet {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+
+
+    opens com.example.projet to javafx.fxml;
+    exports com.example.projet;
+}
